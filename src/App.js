bd0541fd6d98d46be9945ac2c471cf77b927ba85
shrejal joshi <shrejalj10@gmail.com>
2021-06-26 01:04:08 -0700

    Added

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <>
       <NavBar/>
       <Switch>
-        <Route exact path="/" component={Home}/>
+        <Route exact path="resort/" component={Home}/>
         <Route exact path ="/rooms" component={Rooms}/>
         <Route exact path="/rooms/:slug" component={SingleRoom}/>
         <Route component={Error}/>
